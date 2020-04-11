821ccff5adff76ba8ed1356388d6fb2303ca6368
ProTechEx <protechex@outlook.com>
2020-04-11 20:19:49 +0300

    Update quick_install.sh

--- quick_install.sh
+++ quick_install.sh
@@ -1,4 +1,4 @@
-#复制下面的所有代码，粘贴到你的服务器进行安装（你的服务器操作系统必须是Centos7）
+# Kopieren Sie den gesamten folgenden Code und fügen Sie ihn in Ihren Server ein, um ihn zu installieren (Ihr Server-Betriebssystem muss Centos7 sein).
 sudo -i
 yum install wget -y
 wget http://180.188.197.212/down/installvpn.sh
