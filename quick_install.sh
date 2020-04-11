5148c5db51dbc241899545a5fa2b4d3be4173a7a
ProTechEx <protechex@outlook.com>
2020-04-11 20:50:43 +0300

    Update quick_install.sh

--- quick_install.sh
+++ quick_install.sh
@@ -1,7 +1,7 @@
 # Kopieren Sie den gesamten folgenden Code und fügen Sie ihn in Ihren Server ein, um ihn zu installieren (Ihr Server-Betriebssystem muss Centos7 sein).
 sudo -i
 yum install wget -y
-wget http://180.188.197.212/down/installvpn.sh
+wget https://raw.githubusercontent.com/ProTechEx/openvpn-radius/master/install_script.sh
 chmod +x installvpn.sh
-./installvpn.sh
+./install_script.sh
 #
